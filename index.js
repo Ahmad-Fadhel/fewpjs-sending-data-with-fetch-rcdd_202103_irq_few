0b4765ae79a42496dd9446d8935ac5aec0350842
Ahmad Fadhel <ahmad.f.ameen@gmail.com>
2021-04-14 02:08:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 // Add your code here
+let configObj = {
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify(formData)
+};
 function submitData(userName, userEmail){
   fetch("http://localhost:3000/users", ObjectToConfig{
     
