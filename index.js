99fc17f7ea49028beaad3de81a2cd475c5e05f12
Ahmad Fadhel <ahmad.f.ameen@gmail.com>
2021-04-14 02:34:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // Add your code here
-let objConfgc{
+
+
+function submitData(name, email) {
+    return fetch( 'http://localhost:3000/users', {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -9,15 +12,12 @@ let objConfgc{
           name,
           email
         } )
-      } 
-
-function submitData(name, email) {
-    return fetch( 'http://localhost:3000/users', )
+      } )
       .then( function (response) {
         return response.json()
       } )
       .then( function (json) {
-        document.body.innerHTML = json.id
+        document.body.innerHTML = json[ "id" ]
       } )
       .catch( function (err) {
         alert("There was an Error. please try again later");
