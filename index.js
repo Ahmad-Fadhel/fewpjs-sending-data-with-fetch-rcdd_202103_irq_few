49dc384fa49cb521f00ef855dcb301a9eb213278
Ahmad Fadhel <ahmad.f.ameen@gmail.com>
2021-04-14 02:34:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function submitData(name, email) {
         return response.json()
       } )
       .then( function (json) {
-        document.body.innerHTML = json[ "id" ]
+        document.body.innerHTML = json.id;
       } )
       .catch( function (err) {
         alert("There was an Error. please try again later");
